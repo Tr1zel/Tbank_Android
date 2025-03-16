8006dc4f8dbe9eb3d7500fc56af2074ded439d66
Tr1zel <cheapwebsites1@gmail.com>
2025-03-16 17:53:37 +0300

    Добавлен пункт читать в зале, изменена логика(вначале айди), декомпозиция классов

--- First project/Labs/src/Main.kt
+++ First project/Labs/src/Main.kt
@@ -2,17 +2,18 @@ import kotlin.system.exitProcess
 
 fun main() {
     val library = Library()
+    val initlibrary =  initLibrary(library)
     val menus = Menus(library)
-    
-    library.addBook("Гарри Поттер и узник Азкабана", 620, "Джоан Роулинг", true)
-    library.addBook("Унесенные ветром", 270, "Маргарет Митчелл", true)
-    library.addBook("Зеленая миля", 1000, "Стивен Кинг", true)
-    library.addJournal(12345, "Известия Москвы", true)
-    library.addJournal(666, "СпидИнфо", true)
-    library.addJournal(9999, "Комсомольская правда", true)
-    library.addDisk("CD", "Рэмбо 2001", true)
-    library.addDisk("DVD", "Контер страйк 1.6", true)
-    library.addDisk("DVD", "Сборник фильмов Гарри Поттер", true)
+
+    initlibrary.addBook("Гарри Поттер и узник Азкабана", 620, "Джоан Роулинг", true)
+    initlibrary.addBook("Унесенные ветром", 270, "Маргарет Митчелл", true)
+    initlibrary.addBook("Зеленая миля", 1000, "Стивен Кинг", true)
+    initlibrary.addJournal(12345, "Известия Москвы", true)
+    initlibrary.addJournal(666, "СпидИнфо", true)
+    initlibrary.addJournal(9999, "Комсомольская правда", true)
+    initlibrary.addDisk("CD", "Рэмбо 2001", true)
+    initlibrary.addDisk("DVD", "Контер страйк 1.6", true)
+    initlibrary.addDisk("DVD", "Сборник фильмов Гарри Поттер", true)
 
     menus.main_menu()
 }
@@ -45,39 +46,13 @@ class Library {
     val ListJournal: MutableList<LibraryObjects.Journal> = mutableListOf()
     val ListDisk: MutableList<LibraryObjects.Disk> = mutableListOf()
 
-    fun addBook(title: String, pages: Int, author: String, acess: Boolean) {
-        LibraryObjects.Book(Id = ListBook.size, Title = title, Pages = pages, Author = author, Acess = acess)
-            .also { ListBook.add(it) }
-    }
-    fun addJournal(numIssue: Int, title: String, acess: Boolean) {
-        LibraryObjects.Journal(Id = ListJournal.size, NumIssue = numIssue, Title = title, Acess = acess)
-            .also { ListJournal.add(it) }
-    }
-    fun addDisk(typeDisk: String, title: String, acess: Boolean) {
-        LibraryObjects.Disk(Id = ListDisk.size, TypeDisk = typeDisk, Title = title, Acess = acess)
-            .also { ListDisk.add(it) }
-    }
-
-    fun showBook(){
-        println("Показ книг")
-        printInfoShort(1)
-
-    }
-    fun showJournal() {
-        println("Показ газет")
-        printInfoShort(2)
-
-    }
-    fun showDisk() {
-        println("Показ дисков")
-        printInfoShort(3)
-
-    }
-
     fun takeHome(typeId: Int, objectId: Int) {
         val ListToTake = when (typeId) {
             1 -> ListBook
-            2 -> ListJournal
+            2 -> {
+                println("Газету нельзя взять домой!")
+                return
+            }
             3 -> ListDisk
             else -> {
                 println("Неверный typeID")
@@ -95,17 +70,6 @@ class Library {
                             break
                         } else {
                             println("Книги с Id = $objectId нет в наличии, вы не можете ее взять")
-                            return
-                            }
-                    }
-                }
-                2 -> { // Газета
-                    if ((item as LibraryObjects.Journal).Id == objectId) {
-                        if ((item as LibraryObjects.Journal).Acess) {
-                            foundItem = item
-                            break
-                        } else {
-                            println("Газеты с Id = $objectId нет в наличии, вы не можете ее взять")
                             return
                         }
                     }
@@ -128,13 +92,65 @@ class Library {
                 is LibraryObjects.Book  ->  {
                     ListBook[objectId].Acess = ListBook[objectId].Acess.not()
                     println("Книга с Id = $objectId взяли домой")}
-                is LibraryObjects.Journal  -> {
-                    ListJournal[objectId].Acess = ListBook[objectId].Acess.not()
-                    println("Газету с Id = $objectId взяли домой")
-                }
                 is LibraryObjects.Disk  -> {
                     ListDisk[objectId].Acess = ListDisk[objectId].Acess.not()
                     println("Диск с Id = $objectId взяли домой")
+                }
+            }
+        } else {
+            println("Обьект с ID $objectId не найден в списке type_id = $typeId.")
+        }
+    }
+
+    fun readInLibrary(typeId: Int, objectId: Int) {
+        val ListToTake = when (typeId) {
+            1 -> ListBook
+            2 -> ListJournal
+            3 -> {
+                println("Диски нельзя читать в читальном зале!")
+                return
+            }
+            else -> {
+                println("Неверный typeID")
+                return
+            }
+        }
+
+        var foundItem: Any? = null
+        for (item in ListToTake) {
+            when (typeId) {
+                1 -> {  //  Книга
+                    if ((item as LibraryObjects.Book).Id == objectId) {
+                        if ((item as LibraryObjects.Book).Acess) {
+                            foundItem = item
+                            break
+                        } else {
+                            println("Книги с Id = $objectId нет в наличии, вы не можете ее взять")
+                            return
+                        }
+                    }
+                }
+                2 -> {// диск
+                    if ((item as LibraryObjects.Journal).Id == objectId) {
+                        if ((item as LibraryObjects.Journal).Acess) {
+                            foundItem = item
+                            break
+                        } else {
+                            println("Газеты с Id = $objectId нет в наличии, вы не можете ее взять")
+                            return
+                        }
+                    }
+                }
+            }
+        }
+        if (foundItem != null) {
+            when (foundItem) {
+                is LibraryObjects.Book  ->  {
+                    ListBook[objectId].Acess = ListBook[objectId].Acess.not()
+                    println("Книга с Id = $objectId взяли читать в читальный зал")}
+                is LibraryObjects.Journal  -> {
+                    ListJournal[objectId].Acess = ListJournal[objectId].Acess.not()
+                    println("Газету с Id = $objectId взяли читать в читальный зал")
                 }
             }
         } else {
@@ -212,13 +228,110 @@ class Library {
         }
 
     }
+}
+
+class initLibrary(private val library: Library) {
+    val formatprint = FormatPrint(library)
+
+    fun addBook(title: String, pages: Int, author: String, acess: Boolean) {
+        LibraryObjects.Book(Id = library.ListBook.size, Title = title, Pages = pages, Author = author, Acess = acess)
+            .also { library.ListBook.add(it) }
+    }
+    fun addJournal(numIssue: Int, title: String, acess: Boolean) {
+        LibraryObjects.Journal(Id = library.ListJournal.size, NumIssue = numIssue, Title = title, Acess = acess)
+            .also { library.ListJournal.add(it) }
+    }
+    fun addDisk(typeDisk: String, title: String, acess: Boolean) {
+        LibraryObjects.Disk(Id = library.ListDisk.size, TypeDisk = typeDisk, Title = title, Acess = acess)
+            .also { library.ListDisk.add(it) }
+    }
+
+    fun showBook(){
+        println("Показ книг")
+        formatprint.printInfoShort(1)
+    }
+    fun showJournal() {
+        println("Показ газет")
+        formatprint.printInfoShort(2)
+    }
+    fun showDisk() {
+        println("Показ дисков")
+        formatprint.printInfoShort(3)
+    }
+
+}
+
+class Menus(private val library: Library) {
+    private val initLibrary = initLibrary(library)
+    private val formatPrint = FormatPrint(library)
+
+    fun main_menu() {
+        while (true) {
+            println(
+                "1. Показать книги\n" +
+                        "2. Показать газеты\n" +
+                        "3. Показать диски\n" +
+                        "4. Выйти"
+            )
+            println("Выберите пункт меню:")
+
+            when (readlnOrNull()?.toIntOrNull()) {
+                1 -> {
+                    initLibrary.showBook()
+                    second_menu(1)
+                }
+                2 -> {
+                    initLibrary.showJournal()
+                    second_menu(2)
+                }
+                3 -> {
+                    initLibrary.showDisk()
+                    second_menu(3)
+                }
+                4 -> {
+                    println("Спасибо за использование!")
+                    exitProcess(999)
+                }
+                else -> println("Неправильный пункт меню")
+            }
+        }
+    }
+
+    fun second_menu(typeId: Int) {
+        println("Введите Id обьекта с которым хотите работать!")
+        val Object_ID = readlnOrNull()?.toIntOrNull()
+        if (Object_ID == null){
+            println("Id не может быть равным нулю")
+            return
+        }
+        while (true) {
+            println("1. Взять домой\n" +
+                    "2. Читать в читальном зале\n" +
+                    "3. Показать подробную информацию\n" +
+                    "4. Вернуть\n" +
+                    "5. Назад\n")
+            println("Выберите пункт меню:")
+
+            when (readlnOrNull()?.toIntOrNull()) {
+                1 -> library.takeHome(typeId, Object_ID)
+                2 -> library.readInLibrary(typeId, Object_ID)
+                3 -> formatPrint.printInfoLong(typeId, Object_ID)
+                4 -> library.returnFromHome(typeId, Object_ID)
+                5 -> main_menu()
+                else ->  println("Неправильный выбор!")
+            }
+        }
+    }
+}
+
+class FormatPrint(private val library: Library) {
 
     // typeId = Id  типа обьекта, 1 = книги, 2 = газеты, 3 = диски
     fun printInfoShort(typeId: Int) {
         val ListToPrint = when (typeId) {
-            1 -> ListBook
-            2 -> ListJournal
-            3 -> ListDisk
+            1 -> library.ListBook
+            2 -> library.ListJournal
+            3 -> library.ListDisk
             else -> {
                 println("Неверный typeID")
                 return
@@ -236,9 +349,9 @@ class Library {
 
     fun printInfoLong(typeId: Int,  objectId: Int) {
         val ListToPrint = when (typeId) {
-            1 -> ListBook
-            2 -> ListJournal
-            3 -> ListDisk
+            1 -> library.ListBook
+            2 -> library.ListJournal
+            3 -> library.ListDisk
             else -> {
                 println("Неверный typeID")
                 return
@@ -281,67 +394,6 @@ class Library {
                 $TypeDisk $Title доступен: $Acess 
             """.trimIndent()
             else -> "Неизвестный тип"
-        }
-    }
-}
-
-class Menus(private val library: Library) {
-
-    fun main_menu() {
-        while (true) {
-            println(
-                    "1. Показать книги\n" +
-                    "2. Показать газеты\n" +
-                    "3. Показать диски\n" +
-                    "4. Выйти"
-            )
-            println("Выберите пункт меню:")
-
-            when (readlnOrNull()?.toIntOrNull()) {
-                1 -> {
-                    library.showBook()
-                    second_menu(1)
-                    }
-                2 -> {
-                    library.showJournal()
-                    second_menu(2)
-                }
-                3 -> {
-                    library.showDisk()
-                    second_menu(3)
-                }
-                4 -> {
-                    println("Спасибо за использование!")
-                    exitProcess(999)
-                    }
-                else -> println("Неправильный пункт меню")
-            }
-        }
-    }
-
-    fun second_menu(typeId: Int) {
-        println("Введите Id обьекта с которым хотите работать!")
-        val Object_ID = readlnOrNull()?.toIntOrNull()
-        if (Object_ID == null){
-            println("Id не может быть равным нулю")
-            return
-        }
-        while (true) {
-            println("1. Взять домой\n" +
-                    "2. Читать в читальном зале\n" +
-                    "3. Показать подробную информацию\n" +
-                    "4. Вернуть\n" +
-                    "5. Назад\n")
-            println("Выберите пункт меню:")
-
-            when (readlnOrNull()?.toIntOrNull()) {
-                1 -> library.takeHome(typeId, Object_ID)
-                2 -> TODO("Читать в зале")
-                3 -> library.printInfoLong(typeId, Object_ID)
-                4 -> library.returnFromHome(typeId, Object_ID)
-                5 -> main_menu()
-                else ->  println("Неправильный выбор!")
-            }
         }
     }
 }
